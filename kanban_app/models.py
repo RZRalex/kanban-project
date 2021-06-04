9b67d8b56fd6dbe6752981e249b5766dc05994e6
RZRalex <alexxkayne@gmail.com>
2021-06-03 21:11:20 -0700

    login and navbar added

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
+import re, bcrypt
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 USER_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+$')
